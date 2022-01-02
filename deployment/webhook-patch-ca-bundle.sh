6b878302451c73bd5ebd702d0faa46977b6bb8c5
ailton.msj <ailton.msj@gmail.com>
2022-01-02 13:40:47 -0300

    fix kubectl command to get more dinamically the ca-bundle

--- deployment/webhook-patch-ca-bundle.sh
+++ deployment/webhook-patch-ca-bundle.sh
@@ -7,7 +7,7 @@ set -o nounset
 set -o pipefail
 
 
-export CA_BUNDLE=$(kubectl config view --raw --flatten -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."certificate-authority-data"')
+export CA_BUNDLE=$(kubectl config view --raw --flatten -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config view --raw --flatten -o json | jq -r '.contexts[] | select(.name == "'$(kubectl config current-context)'") | .context.cluster')'") | .cluster."certificate-authority-data"')
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
